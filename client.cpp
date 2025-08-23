09f3436c04ea4cfe1741ef3192de458646f7d9fa
Sanjana <sanjanakallu2013@gmail.com>
2025-08-23 07:42:02 +0530

    Debugged server and client side

--- client.cpp
+++ client.cpp
@@ -1,3 +1,66 @@
+// #include <iostream>
+// #include <thread>
+// #include <string>
+
+// #ifdef _WIN32
+// #include <winsock2.h>
+// #include <ws2tcpip.h>
+// #include <windows.h>
+// #pragma comment(lib, "ws2_32.lib") // Link against WinSock
+// #else
+// #include <unistd.h>
+// #include <netinet/in.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+// #endif
+
+
+// void receive_msgs(int sock_fd) {
+//     char buffer[1024];
+//     while (true) {
+//         int bytes = recv(sock_fd, buffer, sizeof(buffer), 0);
+//         if (bytes > 0) std::cout << "\n" << std::string(buffer, bytes) << std::endl;
+//     }
+// }
+
+// int main() {
+
+//     #ifdef _WIN32
+//     WSADATA wsaData;
+//     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+
+//         std::cerr << "WSAStartup failed\n";
+
+//         return 1;
+//     }
+//     #endif
+
+
+//     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+//     sockaddr_in server_addr{AF_INET, htons(9001)};
+//     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//     connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+//     std::thread(receive_msgs, sock_fd).detach();
+
+//     std::string input;
+//     while (true) {
+//         std::getline(std::cin, input);
+//         send(sock_fd, input.c_str(), input.length(), 0);
+//     }
+//     #ifdef _WIN32
+//     closesocket(sock_fd);
+//     #else
+//     close(sock_fd);
+//     #endif
+
+//     #ifdef _WIN32
+//     WSACleanup();
+//     #endif
+
+// }
+
+// client.cpp
 #include <iostream>
 #include <thread>
 #include <string>
@@ -14,27 +77,111 @@
 #include <arpa/inet.h>
 #endif
 
-
 void receive_msgs(int sock_fd) {
     char buffer[1024];
     while (true) {
         int bytes = recv(sock_fd, buffer, sizeof(buffer), 0);
-        if (bytes > 0) std::cout << "\n" << std::string(buffer, bytes) << std::endl;
+        if (bytes == 0) {
+            std::cout << "Disconnected from server" << std::endl;
+            break;
+        }
+#ifdef _WIN32
+        if (bytes == SOCKET_ERROR) {
+            std::cerr << "recv failed with error: " << WSAGetLastError() << std::endl;
+            break;
+        }
+#else
+        if (bytes < 0) {
+            perror("recv failed");
+            break;
+        }
+#endif
+        std::string msg(buffer, bytes);
+        std::cout << "\n" << msg << std::endl;
     }
+#ifdef _WIN32
+    closesocket(sock_fd);
+#else
+    close(sock_fd);
+#endif
 }
 
 int main() {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+#endif
+
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in server_addr{AF_INET, htons(9001)};
+#ifdef _WIN32
+    if (sock_fd == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+#else
+    if (sock_fd < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+#endif
+
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9001);
+#ifdef _WIN32
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+#else
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+#endif
+
+    if (
+#ifdef _WIN32
+        connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR
+#else
+        connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0
+#endif
+    ) {
+#ifdef _WIN32
+        std::cerr << "Connection failed: " << WSAGetLastError() << std::endl;
+        closesocket(sock_fd);
+        WSACleanup();
+#else
+        perror("Connection failed");
+        close(sock_fd);
+#endif
+        return 1;
+    }
 
     std::thread(receive_msgs, sock_fd).detach();
 
     std::string input;
     while (true) {
         std::getline(std::cin, input);
-        send(sock_fd, input.c_str(), input.length(), 0);
+        if (input.empty()) continue; // ignore empty inputs
+        int sent = send(sock_fd, input.c_str(), static_cast<int>(input.length()), 0);
+#ifdef _WIN32
+        if (sent == SOCKET_ERROR) {
+            std::cerr << "send failed with error: " << WSAGetLastError() << std::endl;
+            break;
+        }
+#else
+        if (sent < 0) {
+            perror("send failed");
+            break;
+        }
+#endif
     }
+
+#ifdef _WIN32
     closesocket(sock_fd);
+    WSACleanup();
+#else
+    close(sock_fd);
+#endif
+
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,90 @@
+// #include <iostream>
+// #include <thread>
+// #include <vector>
+// #include <mutex>
+// #include <string>
+
+// #ifdef _WIN32
+// #include <winsock2.h>
+// #include <ws2tcpip.h>
+// #include <windows.h>
+// #pragma comment(lib, "ws2_32.lib") // Link against WinSock
+// #else
+// #include <unistd.h>
+// #include <netinet/in.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+// #endif
+
+// // #include <unistd.h>
+// // #include <netinet/in.h>
+
+// std::vector<int> clients;
+// std::mutex clients_mutex;
+
+// void broadcast(const std::string& msg, int sender_fd) {
+//     std::lock_guard<std::mutex> lock(clients_mutex);
+//     for (int client_fd : clients) {
+//         if (client_fd != sender_fd) {
+//             send(client_fd, msg.c_str(), msg.length(), 0);
+//         }
+//     }
+// }
+
+// void handle_client(int client_fd) {
+//     char buffer[1024];
+//     while (true) {
+//         int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+//         if (bytes <= 0) break;
+//         std::string msg(buffer, bytes);
+//         broadcast(msg, client_fd);
+//     }
+//     #ifdef _WIN32
+//     closesocket(client_fd);
+//     #else
+//     close(client_fd);
+//     #endif
+
+// }
+
+// int main() {
+//     #ifdef _WIN32
+// WSADATA wsaData;
+// if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+//     std::cerr << "WSAStartup failed\n";
+//     return 1;
+// }
+// #endif
+
+
+//     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+//     sockaddr_in address{AF_INET, htons(9001), INADDR_ANY};
+//     bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+//     listen(server_fd, 5);
+
+//     while (true) {
+//         sockaddr_in cli_addr;
+//         socklen_t len = sizeof(cli_addr);
+//         int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &len);
+//         {
+//             std::lock_guard<std::mutex> lock(clients_mutex);
+//             clients.push_back(client_fd);
+//         }
+//         std::thread(handle_client, client_fd).detach();
+//     }
+//     #ifdef _WIN32
+//     WSACleanup();
+//     #endif
+
+// }
+
+// server.cpp
 #include <iostream>
 #include <thread>
 #include <vector>
 #include <mutex>
 #include <string>
+#include <algorithm>
 
 #ifdef _WIN32
 #include <winsock2.h>
@@ -16,9 +98,6 @@
 #include <arpa/inet.h>
 #endif
 
-// #include <unistd.h>
-// #include <netinet/in.h>
-
 std::vector<int> clients;
 std::mutex clients_mutex;
 
@@ -26,7 +105,18 @@ void broadcast(const std::string& msg, int sender_fd) {
     std::lock_guard<std::mutex> lock(clients_mutex);
     for (int client_fd : clients) {
         if (client_fd != sender_fd) {
-            send(client_fd, msg.c_str(), msg.length(), 0);
+            int sent = send(client_fd, msg.c_str(), static_cast<int>(msg.length()), 0);
+            if (sent == -1
+#ifdef _WIN32
+                || sent == SOCKET_ERROR
+#endif
+            ) {
+#ifdef _WIN32
+                std::cerr << "send failed with error: " << WSAGetLastError() << std::endl;
+#else
+                perror("send failed");
+#endif
+            }
         }
     }
 }
@@ -35,28 +125,142 @@ void handle_client(int client_fd) {
     char buffer[1024];
     while (true) {
         int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes <= 0) break;
+        if (bytes == 0) {
+            std::cout << "Client disconnected" << std::endl;
+            break;
+        }
+#ifdef _WIN32
+        if (bytes == SOCKET_ERROR) {
+            std::cerr << "recv failed with error: " << WSAGetLastError() << std::endl;
+            break;
+        }
+#else
+        if (bytes < 0) {
+            perror("recv failed");
+            break;
+        }
+#endif
         std::string msg(buffer, bytes);
+        std::cout << "Received from client: " << msg << std::endl;
         broadcast(msg, client_fd);
     }
+
+#ifdef _WIN32
     closesocket(client_fd);
-    // Remove from clients list...
+#else
+    close(client_fd);
+#endif
+
+    {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+        clients.erase(std::remove(clients.begin(), clients.end(), client_fd), clients.end());
+    }
 }
 
 int main() {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+#endif
+
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in address{AF_INET, htons(9001), INADDR_ANY};
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-    listen(server_fd, 5);
+#ifdef _WIN32
+    if (server_fd == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+#else
+    if (server_fd < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+#endif
+
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_port = htons(9001);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    if (
+#ifdef _WIN32
+        bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR
+#else
+        bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0
+#endif
+    ) {
+#ifdef _WIN32
+        std::cerr << "Bind failed: " << WSAGetLastError() << std::endl;
+        closesocket(server_fd);
+        WSACleanup();
+#else
+        perror("Bind failed");
+        close(server_fd);
+#endif
+        return 1;
+    }
+
+
+    if (
+#ifdef _WIN32
+        listen(server_fd, 5) == SOCKET_ERROR
+#else
+        listen(server_fd, 5) < 0
+#endif
+    ) {
+#ifdef _WIN32
+        std::cerr << "Listen failed: " << WSAGetLastError() << std::endl;
+        closesocket(server_fd);
+        WSACleanup();
+#else
+        perror("Listen failed");
+        close(server_fd);
+#endif
+        return 1;
+    }
+
+    std::cout << "Server started and listening on port 9001" << std::endl;
 
     while (true) {
-        sockaddr_in cli_addr;
+        sockaddr_in cli_addr{};
         socklen_t len = sizeof(cli_addr);
-        int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &len);
+        int client_fd =
+#ifdef _WIN32
+            accept(server_fd, (struct sockaddr*)&cli_addr, &len);
+#else
+            accept(server_fd, (struct sockaddr*)&cli_addr, &len);
+#endif
+
+#ifdef _WIN32
+        if (client_fd == INVALID_SOCKET) {
+            std::cerr << "Accept failed: " << WSAGetLastError() << std::endl;
+            continue;
+        }
+#else
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+#endif
+
         {
             std::lock_guard<std::mutex> lock(clients_mutex);
             clients.push_back(client_fd);
         }
+
         std::thread(handle_client, client_fd).detach();
     }
+
+#ifdef _WIN32
+    closesocket(server_fd);
+    WSACleanup();
+#else
+    close(server_fd);
+#endif
+
+    return 0;
 }
+
